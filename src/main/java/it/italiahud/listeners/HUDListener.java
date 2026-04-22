30807ba29e1c534fc8d859a2d9a28b90f9ba6205
luigianck1312-gif <luigianck1312@gmail.com>
2026-04-22 23:36:25 +0200

    Add files via upload

---
+++ src/main/java/it/italiahud/listeners/HUDListener.java
@@ -0,0 +1,38 @@
+package it.italiahud.listeners;
+
+import it.italiahud.ItaliaHUD;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class HUDListener implements Listener {
+
+    private final ItaliaHUD plugin;
+
+    public HUDListener(ItaliaHUD plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        plugin.getScoreboardManager().setupScoreboard(e.getPlayer());
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        plugin.getScoreboardManager().removeScoreboard(e.getPlayer());
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        Player victim = e.getEntity();
+        Player killer = victim.getKiller();
+        plugin.getScoreboardManager().addDeath(victim.getUniqueId());
+        if (killer != null && !killer.equals(victim)) {
+            plugin.getScoreboardManager().addKill(killer.getUniqueId());
+        }
+    }
+}
